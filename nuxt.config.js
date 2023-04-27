34b1cf73bea034de13641920744ededc44c213af
Renan <82916964+thisisrenan@users.noreply.github.com>
2023-04-27 20:47:33 -0300

    Update nuxt.config.js

--- nuxt.config.js
+++ nuxt.config.js
@@ -63,7 +63,7 @@ export default {
 
   // Axios module configuration: https://go.nuxtjs.dev/config-axios
   axios: {
-    baseURL: 'http://localhost:3000'
+    baseURL: process.env.API_BASE_URL
   },
 
   proxy: {
