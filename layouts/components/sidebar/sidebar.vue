14ebb2366b2512e477e1369e03d05db92b78cb50
thisisrenan <joserenandasilva08@gmail.com>
2023-06-12 19:53:43 -0300

    navbar nova

--- layouts/components/sidebar/sidebar.vue
+++ layouts/components/sidebar/sidebar.vue
@@ -1,42 +1,5 @@
 <template>
-  <div class="
-        fixed
-        bg-white
-        transition-all
-        duration-500
-        h-screen
-        divide-y divide-gray-200 divide-solid
-        border-r
-      " v-if="collapsed == true" style="width: 5%">
-    <el-menu class="h-screen" :collapse="!collapsed" background-color="#C94F32" text-color="#ffffff"
-      active-text-color="#34512a" @select="menuSelect">
-      <!-- menu -->
-      <img src="~/assets/public/logo.png" id="logo">
-      <nuxt-link v-if="user.isadm === true" to="/jogos">
-        <el-tooltip class="item" content="Jogos" effect="light" placement="left-start">
-          <el-menu-item index="jogos">
-            <div class="inline-block align-middle">
-              <i-game align-middle theme="outline" size="20" fill="#ffffff" :strokeWidth="2" />
-            </div>
-          </el-menu-item> </el-tooltip></nuxt-link>
 
-      <nuxt-link v-if="user.isadm === true" to="/lojas">
-        <el-tooltip class="item" content="Lojas" effect="light" placement="left-start">
-          <el-menu-item index="lojas">
-            <div class="inline-block align-middle">
-              <i-shop theme="outline" size="20" fill="#ffffff" :strokeWidth="2" />
-            </div>
-          </el-menu-item> </el-tooltip></nuxt-link>
-
-      <nuxt-link to="/jogos/compara">
-        <el-tooltip class="item" content="Comparar preços" effect="light" placement="left-start">
-          <el-menu-item index="compara">
-            <div class="inline-block align-middle">
-              <i-arithmetic-one theme="outline" size="20" fill="#ffffff" :strokeWidth="2" />
-            </div>
-          </el-menu-item> </el-tooltip></nuxt-link>
-    </el-menu>
-  </div>
 </template>
 
 <style>
