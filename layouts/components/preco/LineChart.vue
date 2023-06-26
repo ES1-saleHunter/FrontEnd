428b060208c0eb378f8b93fa230ef8512bd19491
thisisrenan <joserenandasilva08@gmail.com>
2023-06-26 20:47:36 -0300

    Corrigindo erro do grafico

--- layouts/components/preco/LineChart.vue
+++ layouts/components/preco/LineChart.vue
@@ -1,11 +1,26 @@
 <script>
 import {Line} from 'vue-chartjs'
+import { mixins } from 'vue-chartjs'
+const { reactiveProp } = mixins
 export default {
     extends: Line,
     props: ['chartData', "options"],
-    mounted(){
-        this.renderChart(this.chartData, this.options);
+    mixins: [reactiveProp],
+    watch: {
+    chartData () {
+      this.renderChart(this.chartData, this.options)
     }
+  },
+    mounted(){
+        this.alldata()
+    },
+    methods: {
+        alldata(){
+            this.renderChart(this.chartData, this.options)
+        }
+    },
+   
+
 }
 
 </script>
